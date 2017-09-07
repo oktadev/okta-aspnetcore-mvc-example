587c7b362bd7ef554ac267ba896134efaf744043
Nate Barbettini <nathanaelb@gmail.com>
2017-09-06 17:00:42 -0700

    Create project

---
+++ OktaAspNetCoreMvc/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OktaAspNetCoreMvc.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            if (!HttpContext.User.Identity.IsAuthenticated)
+            {
+                return Challenge(OpenIdConnectDefaults.AuthenticationScheme);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Logout()
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                return SignOut(CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
