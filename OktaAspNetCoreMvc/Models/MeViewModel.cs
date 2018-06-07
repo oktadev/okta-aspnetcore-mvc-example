2a4e5a47fdb667d4e1c775aeecfde8e2dde7d2d1
Nate Barbettini <nathanaelb@gmail.com>
2018-06-07 13:49:04 -0500

    Clean up and prettify the example

---
+++ OktaAspNetCoreMvc/Models/MeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace OktaAspNetCoreMvc.Models
+{
+    public class MeViewModel
+    {
+        public string Username { get; set; }
+
+        public bool SdkAvailable { get; set; }
+
+        public dynamic UserInfo { get; set; }
+
+        public IEnumerable<string> Groups { get; set; }
+    }
+}
